0ccdd08ada9bde8dcf65d3a522915e705cfe77e2
Ponomarev Stepan <htconesuper11@gmail.com>
2020-05-27 13:41:47 +0300

    removed front src

--- frontend/src/store.js
+++
@@ -1,3 +0,0 @@
-import {writable} from "svelte/store";
-
-export const USER = writable(null);
--- frontend/src/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-const DOMAIN = 'http://localhost:8080'
-
-export async function doQuery(url, opt) {
-    return await fetch(DOMAIN + url, opt);
-}
